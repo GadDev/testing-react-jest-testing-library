46c9fc9669635ccbaf026e6b59c55056f3db2b47
algadaix <alexandre@qured.com>
2021-03-08 12:07:56 +0000

    more style, and adding test for enable-button-checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import "./App.css";
 
 import { useState } from "react";
 
 function App() {
   const [btnColor, setBtnColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
   const btnName = btnColor === "red" ? "blue" : "red";
   return (
     <div className="App">
@@ -12,9 +14,21 @@ function App() {
           type="button"
           style={{ backgroundColor: btnColor }}
           onClick={() => setBtnColor(btnName)}
+          disabled={disabled}
         >
           Change to {btnName}
         </button>
+        <div className="checkbox-field">
+          <input
+            type="checkbox"
+            id="enable-button-checkbox"
+            name="enable-button-checkbox"
+            defaultChecked={disabled}
+            aria-checked={disabled}
+            onChange={(e) => setDisabled(e.target.checked)}
+          />
+          <label htmlFor="enable-button-checkbox">Disable button</label>
+        </div>
       </main>
     </div>
   );
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -17,3 +17,31 @@ describe("Button", () => {
     expect(colorBtn.textContent).toBe("Change to red");
   });
 });
+
+describe("Checkbox", () => {
+  test("initial conditions", () => {
+    render(<App />);
+    // check that the button starts out enabled
+    const coloredBtn = screen.getByRole("button", {
+      name: "Change to blue",
+    });
+    expect(coloredBtn).toBeEnabled();
+    // check that the checkbox starts out unchecked
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).not.toBeChecked();
+  });
+
+  test("Button is disabled when the checkbox is checked and the button is enabled if the checkbox is unchecked", () => {
+    render(<App />);
+    const coloredBtn = screen.getByRole("button", {
+      name: "Change to blue",
+    });
+    const checkbox = screen.getByRole("checkbox");
+
+    fireEvent.click(checkbox);
+    expect(coloredBtn).toBeDisabled();
+
+    fireEvent.click(checkbox);
+    expect(coloredBtn).toBeEnabled();
+  });
+});
